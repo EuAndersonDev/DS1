ffeff795e833b40f12a2107e89b0b1fe2faf08cc
Anderson Reis <137122362+AndersonReis04@users.noreply.github.com>
2024-04-02 21:57:36 -0300

    Update Main.java

--- ExercicioEncapsulamento/src/Main.java
+++ ExercicioEncapsulamento/src/Main.java
@@ -3,20 +3,27 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		
 		Produto p1 = new Produto();
+		String resposta = "SIM";
 		
-		System.out.println("Coloque o nome do produto:");
-		p1.setNome(in.next());
+		while(! resposta.equalsIgnoreCase("NÃO")){
+			System.out.println("Coloque o nome do produto:");
+			p1.setNome(in.next());
+			
+			System.out.println("Coloque o preço de custo:");	
+			p1.setPrecoCusto(in.nextDouble());
+			
+			System.out.println("Coloque o preço de venda:");
+			p1.setPrecoVenda(in.nextDouble());
+			while(p1.getPrecoVenda() < p1.getPrecoCusto()) {
+				teste1.setPrecoVenda(in.nextDouble());
+			}
+		}
 		
-		System.out.println("Coloque o preço de custo:");	
-		p1.setPrecoCusto(in.nextDouble());
-		
-		System.out.println("Coloque o preço de venda:");
-		p1.setPrecoVenda(in.nextDouble());
-		
+	System.out.println("");	
         System.out.println("Produto: " + p1.getNome());
         System.out.printf("Lucro: R$%.2f\n", p1.calcularMargemLucro());
         System.out.printf("Porcentagem de lucro: %.2f%%\n", p1.getMargemLucroPorcentagem());
+	System.out.println("";
     }
 }
