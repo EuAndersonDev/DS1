2581604aa2266286cfa437d3c837c7b8cc8d5d34
Anderson Reis <137122362+AndersonReis04@users.noreply.github.com>
2024-04-16 08:33:46 -0300

    Add files via upload

---
+++ ExSobrecarga/src/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Professor Prof1 = new Professor(11, "DONI");
+		System.out.println(Prof1);
+	}
+}
---
+++ ExSobrecarga/src/Professor.java
@@ -0,0 +1,17 @@
+
+public class Professor {
+	int codigo;
+	String nome;
+	String diciplina;
+	String turno;
+
+	
+	public Professor() {
+	}
+	
+	public Professor(int cod, String nom) {
+		this.codigo = cod;
+		this.nome = nom;
+	}
+	
+}
