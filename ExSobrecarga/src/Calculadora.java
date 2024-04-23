4d4999bc6dc282a6c21ac9c0e065af899b8ce2fc
Anderson Reis <137122362+AndersonReis04@users.noreply.github.com>
2024-04-23 18:33:03 -0300

    Add files via upload

--- ExSobrecarga/src/Calculadora.java
+++ ExSobrecarga/src/Calculadora.java
@@ -1,57 +1,72 @@
+import java.util.ArrayList;
 import java.util.List;
 public class Calculadora {
-    // Atributos
+	
     private String nome;
     private double resultado;
     
-    // Métodos get e set
+    //gets e sets
     public String getNome() {
         return nome;
     }
+    
     public void setNome(String nome) {
         this.nome = nome;
     }
+    
     public double getResultado() {
         return resultado;
     }
+    
     public void setResultado(double resultado) {
         this.resultado = resultado;
     }
-
-    // Método de soma inteiros e decimais
+    
+    // Metodos de soma
     public void soma(int n1, int n2) {
         resultado = n1 + n2;
     }
     public void soma(double n1, double n2){
         resultado = n1 + n2;
     }
-
-    //Método de subtração inteiros e decimais
+    
+    public void soma(Number n1, Number n2){
+        resultado = n1.doubleValue() + n2.doubleValue();
+    }
+    
+    public void soma(List<Double> numeros) {
+        double sum = 0;
+        for (double num : numeros) {
+            sum += num;
+        }
+        resultado = sum;
+    }
+    
+    //Metodo de subtracao
     public void subtracao(int n1, int n2){
         resultado = n1 - n2;
     }
     public void subtracao(double n1, double n2){
         resultado = n1 - n2;
     }
-
-    // Método de soma para uma lista de números
-    public void soma(List<Double> numeros) {
-        resultado = 0;
-        for (Double num : numeros) {
-            resultado += num;
-        }
+    
+    public void subtracao(Number n1, Number n2){
+        resultado = n1.doubleValue() - n2.doubleValue();
     }
-
-    // Método de subtração para uma lista de números
+    
     public void subtracao(List<Double> numeros) {
-        if ( ! numeros.isEmpty() ) {
-            resultado = numeros.get(0);
+        if (numeros.size() > 0) {
+            double sub = numeros.get(0);
             for (int i = 1; i < numeros.size(); i++) {
-                resultado -= numeros.get(i);
+                sub -= numeros.get(i);
             }
+            resultado = sub;
+        } else {
+            System.out.println("Lista de números vazia ou inválida");
+            resultado = 0;
         }
     }
-
-
+    
+    
+	
 }
-
--- ExSobrecarga/src/Main.java
+++ ExSobrecarga/src/Main.java
@@ -1,8 +1,67 @@
-
+import java.util.List;
+import java.util.ArrayList;
+import java.util.*;
 public class Main {
+
 	public static void main(String[] args) {
 		
-		Professor Prof1 = new Professor(11, "DONI");
-		System.out.println(Prof1);
+		Scanner in = new Scanner(System.in);
+		Calculadora calc = new Calculadora();
+		int operacao, n = 0;
+		double num1, num2;
+		
+        System.out.println("Escolha sua operação");
+        System.out.println("Digite 1 para soma de dois numeros");
+        System.out.println("Digite 2 para subtracao de dois numeros");
+        System.out.println("Digite 3 para a soma de uma lista de numeros");
+        System.out.println("Digite 4 para a subtracao de uma lista de numeros");
+        operacao = in.nextInt();
+		
+        
+        switch(operacao) {
+	        case 1:
+	            System.out.println("Digite dois números para soma:");
+	            num1 = in.nextDouble();
+	            num2 = in.nextDouble();
+	            calc.soma(num1, num2);
+	            System.out.println("Soma: " + calc.getResultado());
+	            break;
+	        case 2:
+	            System.out.println("Digite dois números para subtrai-los:");
+	            num1 = in.nextDouble();
+	            num2 = in.nextDouble();
+	            calc.subtracao(num1, num2);
+	            System.out.println("Subtracao: " + calc.getResultado());
+	        	break;
+	        case 3:
+	            System.out.println("Soma de uma lista de numeros. Digite quantos números deseja somar:");
+	            n = in.nextInt();
+	            List<Double> numerosS = new ArrayList<>();
+	            System.out.println("Digite os numeros:");
+	            for (int i = 0; i < n; i++) {
+	                numerosS.add(in.nextDouble());
+	            }
+	            calc.soma(numerosS);
+	            System.out.println("Resultado: " + calc.getResultado());
+
+	        	break;
+	        case 4:
+	            System.out.println("Subtração de uma lista de números. Digite quantos números deseja subtrair:");
+	            n = in.nextInt();
+	            List<Double> numerosSub = new ArrayList<>();
+	            System.out.println("Digite os números:");
+	            for (int i = 0; i < n; i++) {
+	                numerosSub.add(in.nextDouble());
+	            }
+	            calc.subtracao(numerosSub);
+	            System.out.println("Resultado: " + calc.getResultado());
+	            break;
+	        default:
+	        	System.out.println("Essa opção não existe!");
+	        	break;
+        }
+
+		
 	}
+
 }
